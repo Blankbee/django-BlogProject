c3a7de8b2af6e4f63394e90b4d5fe53165e8487b
Blankbee <shen.esad@outlook.com>
2023-02-06 20:25:44 +0300

    created register form

---
+++ user/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class RegisterForm(forms.Form):
+    username=forms.CharField(max_length=50,label="User Name")
+    password=forms.CharField(max_length=20,label="Password",widget=forms.PasswordInput)
+    confirm=forms.CharField(max_length=20,label="Confirm Password",widget=forms.PasswordInput)
+    
+    def clean(self):    # bu fonksiyon göndereceğimiz bilgileri göndermeden önce onları alıp işlem yapmamızı sağlar.
+        username=self.cleaned_data.get("username")
+        password=self.cleaned_data.get("password")
+        confirm=self.cleaned_data.get("confirm")
+
+        if password and confirm and password !=confirm:
+            raise forms.ValidationError("Passwords not matching!")
+        
+        values={
+            "username":username,
+            "password":password
+        }
+        return values
+    
