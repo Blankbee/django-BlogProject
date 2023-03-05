b0fa8e98f0204ff315fd8792f698e394a0ca4431
Blankbee <shen.esad@outlook.com>
2023-03-05 20:59:13 +0300

    login_required added

--- article/views.py
+++ article/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render,redirect,get_object_or_404
 from .forms import ArticleForm
 from django.contrib import messages
 from .models import Article
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
 def about(request):
     return render(request,"about.html")
+@login_required(login_url="user:login")
 def dashboard(request):
     articles=Article.objects.filter(author=request.user)
     context={
         "articles":articles
     }
     return render(request,"dashboard.html",context)
+@login_required(login_url="user:login")
 def addArticle(request):#Aynı formu oluştururken olduğu gibi appin modeli olduğundan kolayca ona kaydeder.
     form=ArticleForm(request.POST or None,request.FILES or None)
     if form.is_valid():
@@ -25,10 +30,12 @@ def addArticle(request):#Aynı formu oluştururken olduğu gibi appin modeli old
 
 
     return render(request,"addarticle.html",{"form":form})
+
 def detail(request,id):
     #article=Article.objects.filter(id=id).first()
     article=get_object_or_404(Article,id=id)
     return render(request,"detail.html",{"article":article})
+@login_required
 def updateArticle(request,id):
     article=get_object_or_404(Article, id=id)
     form=ArticleForm(request.POST or None,request.FILES or None,instance=article)
@@ -40,6 +47,7 @@ def updateArticle(request,id):
         return redirect("dashboard")
 
     return render(request,"update.html",{"form":form})
+@login_required
 def deleteArticle(request,id):
     article=get_object_or_404(Article,id=id)
     article.delete()
