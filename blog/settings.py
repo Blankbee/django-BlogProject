5e9373f03033d1203be52740537f35332dd54097
Esad Sen <90692939+esadsen@users.noreply.github.com>
2024-07-14 18:35:08 +0300

    "Set DEBUG to False in settings.py"

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-945jfx1%gq!d41p&sl)b2cj13#s@!7=!!mjsba%t(f-&+06@m6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1",
                  "localhost",
